a0dd65bedd6e286555bf0ca8e5c74545c5fbab74
GalinaAlnichenkova <g.alnichenkova@gmail.com>
2022-11-05 20:56:11 +0400

    Homework3

---
+++ Homework_3/1.js
@@ -0,0 +1,3 @@
+let degreesС = prompt('Введите температуру в градусах Цельсия');
+let degreesF = parseFloat((9/5) * degreesС + 32).toFixed(1);
+alert(`Цельсий: ${degreesС}, Фаренгейт: ${degreesF}`);
---
+++ Homework_3/2.js
@@ -0,0 +1,5 @@
+const getName = prompt("Введите Ваше имя: ");
+userName(getName);
+function userName(firstNane) {
+  console.log(`Привет, ${firstNane}!`);
+}
